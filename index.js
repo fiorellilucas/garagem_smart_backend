9bfe65a7e617233386e0cadeb8f60b2b11076d80
Lucas Fiorelli <fiorellilucas@gmail.com>
2025-04-20 15:07:54 -0300

    Remover query separada que não estava sendo utilizada

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require('@prisma/client')
-const { getEstabelecimentosComValor, getReservationModalData } = require('@prisma/client/sql')
+const { getEstabelecimentosComValor } = require('@prisma/client/sql')
 const express = require('express')
 const cors = require('cors')
 
